1e593a290bcae1107ac5c9e3cff4b34314125051
Kage8eebdjW <137019241+Kage8eebdjW@users.noreply.github.com>
2024-11-01 19:03:34 +0800

    Update 1017qo917103592.lua

--- refs/heads/main/1017qo917103592.lua
+++ refs/heads/main/1017qo917103592.lua
@@ -2,4 +2,4 @@ getgenv().AuthKey = "HUGE_73hRxG7zwEmR"
 getgenv().LoadSettings = {
     Example_Setting = Example_Value
 }
-loadstring(game:HttpGet("https://HugeGames.io/ps99"))(
+loadstring(game:HttpGet("https://HugeGames.io/ps99"))()
